1373e982ad9561a7f525a06d93c2ae8ea457488f
alvaro villalobos <alvaro.villalobos@social-it.cl>
2022-11-11 15:38:07 -0300

    tetete

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -3,9 +3,9 @@ import {pool} from '../database/db.js'
 export const getProducts = async (req, res)=> {
     try{
         // throw new Error('test') prueba de error 
-        const [product] = await pool.query('SELECT p.name, p.category, c.name FROM product p INNER JOIN category c ON p.category=c.id')
+        const [produ] = await pool.query('SELECT * FROM product p INNER JOIN category c ON p.category=c.id')
         // const [product] = await pool.query('SELECT * FROM category')
-        res.json(product)
+        res.json(produ)
     }catch(errro){
         return res.status(500).json({
             message: 'error db'
